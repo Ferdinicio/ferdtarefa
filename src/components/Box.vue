1ec14f31ec79d0bb25ef3304255e4f348cbd4ce3
ferdinicio <ferdinicio@gmail.com>
2022-10-01 19:14:22 -0300

    Projeto vue para gerenciar tarefas

---
+++ src/components/Box.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box has-text-weight-bold" :style="estilos">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Box',
+  data () {
+    return {
+      estilos: {
+        backgroundColor: '#baeaff'
+      }
+    }
+  }
+})
+</script>
