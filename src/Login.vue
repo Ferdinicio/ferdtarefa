cd0e19b20dac273cb8f80d2ec1b86f566fce96f8
ferdinicio <ferdinicio@gmail.com>
2022-10-01 21:17:44 -0300

    Criação de tela de login

---
+++ src/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <main class="box formulario" aling="center" >
+  <div class="box formulario" aling="center">
+    <div class="field">
+  <label class="label">Login</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input" type="text" placeholder="Login" value="">
+    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </div>
+</div>
+<div class="field">
+  <div class="control has-icons-left has-icons-right">
+    <input class="input" type="password" placeholder="Senha">
+    <span class="icon is-small is-left">
+      <i class="fas fa-lock"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </div>
+</div>
+ 
+  </div>
+</main>
+</template>
+
+<script lang="ts">
+
+
+</script>
+<style>
+main {
+  color: #2a2a3d;
+  background-color: #7d7d7d;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './Login.vue'
 
 import '@fortawesome/fontawesome-free/css/all.css'
 
