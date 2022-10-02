c7f2160d9a26ccb3a4106333c4778b1818425ec2
ferdinicio <ferdinicio@gmail.com>
2022-10-02 13:43:25 -0300

    Criação de rotas e views para nova versao

---
+++ src/views/Tarefas.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="column is-three-quarter conteudo">
+      <Formulario @aoSalvarTarefa="salvarTarefa" />
+      <div class="lista">
+          <Box v-if="listaEstaVazia">
+              Lista de tarefas vazia!
+          </Box>
+          <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+      </div>
+  </div>
+</template>
+  
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import Formulario from '../components/Formulario.vue'
+  import Tarefa from '../components/Tarefa.vue'
+  import Box from '../components/Box.vue'
+  import ITarefa from '../interfaces/ITarefa'
+  
+  export default defineComponent({
+    name: 'App',
+    components: {
+      Formulario,
+      Tarefa,
+      Box
+    },
+    data () {
+      return {
+        tarefas: [] as ITarefa[]
+      }
+    },
+    computed: {
+      listaEstaVazia () : boolean {
+        return this.tarefas.length === 0
+      }
+    },
+    methods: {
+      salvarTarefa (tarefa: ITarefa) {
+        this.tarefas.push(tarefa)
+      }
+    }
+  });
+  </script>
+  
